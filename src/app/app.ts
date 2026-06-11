fe32b05673eeaf51e76d88164d526672afdbe5ef
Bjoern <onoofelogmail.com>
2026-06-11 10:09:25 +0200

    - imported "header" & "navbar" to app.ts; - added "rootDIR" to stconfig.app.json;

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header'
+import { NavBar } from './components/nav-bar/nav-bar'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Header,NavBar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
