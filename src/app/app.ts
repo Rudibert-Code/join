3d2a75ca2671b7b0eeb84f9a1af57818dc795ab5
Bjoern <onoofelogmail.com>
2026-06-11 17:00:47 +0200

    - sorting array TEST

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,11 @@
 import { Component, signal, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from './components/header/header'
-import { NavBar } from './components/nav-bar/nav-bar'
 import { Supabase } from './supabase';
 import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Header,NavBar,JsonPipe],
+  imports: [RouterOutlet,JsonPipe],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
