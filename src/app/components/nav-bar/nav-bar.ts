49ae9b16c8c7fad0e5f6fefc5555ab57572115cc
Bjoern <onoofelogmail.com>
2026-06-11 11:13:41 +0200

    re-wrote footer script : - icon informations now located in nav-bar.ts - icons now generate via @for

--- src/app/components/nav-bar/nav-bar.ts
+++ src/app/components/nav-bar/nav-bar.ts
@@ -1,9 +1,38 @@
 import { Component } from '@angular/core';
 
+interface Functions{
+  img:String,
+  name:String,
+  id:String
+}
+
 @Component({
   selector: 'app-nav-bar',
   imports: [],
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.scss',
 })
-export class NavBar {}
+export class NavBar {
+  function:Functions[]=[
+    {
+      img:'/assets/UI/vector/icon_summary.svg',
+      name:'Summary',
+      id:'summary'
+    },
+    {
+      img:'/assets/UI/vector/icon_add-task.svg',
+      name:'Add Tasks',
+      id:'addtasks'
+    },
+    {
+      img:'/assets/UI/vector/icon_board.svg',
+      name:'Board',
+      id:'board'
+    },
+    {
+      img:'/assets/UI/icon_contacts.png',
+      name:'Contacts',
+      id:'contacs'
+    },
+  ]
+}
