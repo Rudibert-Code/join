fd2b4fa24bf20c449a10fbbbb608f53e6cab941b
Felix Winkler <Felix-Winkler96@web.de>
2026-06-15 15:30:33 +0200

    added legal / policy (internal)

--- src/app/pages/legal-notice/legal-notice.ts
+++ src/app/pages/legal-notice/legal-notice.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Header } from "../../components/header/header";
 
 @Component({
   selector: 'app-legal-notice',
-  imports: [],
+  imports: [Header],
   templateUrl: './legal-notice.html',
   styleUrl: './legal-notice.scss',
 })
