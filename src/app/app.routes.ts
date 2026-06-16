15e5e477e7015964d9f9ee2d992f37d42ff21b35
Jana S. <schaaf.jana@outlook.de>
2026-06-16 10:10:10 +0200

    add routes in routes.ts and navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
 import { ContactList } from './pages/contact-list/contact-list';
-import { Dummy } from './pages/dummy/dummy';
-import { LogIn } from './components/log-in/log-in';
 import { Summery } from './pages/summery/summery';
 import { AddTask } from './pages/add-task/add-task';
 import { Board } from './pages/board/board';
 import { MainLayout } from './layouts/main-layout/main-layout';
+import { LogIn } from './pages/log-in/log-in';
+import { LegalNotice } from './pages/legal-notice/legal-notice';
+import { PrivacyPolicy } from './pages/privacy-policy/privacy-policy';
 
 export const routes: Routes = [
   {
@@ -21,6 +22,8 @@ export const routes: Routes = [
       { path: 'add-task', component: AddTask },
       { path: 'board', component: Board },
       { path: 'contact-list', component: ContactList },
+      { path: 'legal-notice', component: LegalNotice },
+      { path: 'privacy-police', component: PrivacyPolicy },
     ],
   },
 ];
