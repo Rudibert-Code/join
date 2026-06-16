0b956e1fa00cba12ea81ee9057ff903735a49ae4
Felix Winkler <Felix-Winkler96@web.de>
2026-06-16 09:14:29 +0200

    added summary

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,31 +1,36 @@
 import { Routes } from '@angular/router';
 import { ContactList } from './pages/contact-list/contact-list';
-import { Dummy } from './pages/dummy/dummy'
+import { Dummy } from './pages/dummy/dummy';
 import { LogIn } from './pages/log-in/log-in';
 import { LegalNotice } from './pages/legal-notice/legal-notice';
 import { PrivacyPolicy } from './pages/privacy-policy/privacy-policy';
+import { Summary } from './pages/summary/summary';
 
 export const routes: Routes = [
-    {
-        path:'',
-        component: Dummy
-        //redirectTo: 'login',
-        //pathMatch: 'full'
-    },
-    {
-        path:'contact-list',
-        component:ContactList
-    },
-    {
-        path:"login",
-        component:LogIn
-    },
-        {
-        path:'login/legal',
-        component:LegalNotice
-    },
-    {
-        path:"login/policy",
-        component:PrivacyPolicy
-    }
+  {
+    path: '',
+    component: Dummy,
+    //redirectTo: 'login',
+    //pathMatch: 'full'
+  },
+  {
+    path: 'contact-list',
+    component: ContactList,
+  },
+  {
+    path: 'login',
+    component: LogIn,
+  },
+  {
+    path: 'login/legal',
+    component: LegalNotice,
+  },
+  {
+    path: 'login/policy',
+    component: PrivacyPolicy,
+  },
+  {
+    path: 'summary',
+    component: Summary,
+  },
 ];
