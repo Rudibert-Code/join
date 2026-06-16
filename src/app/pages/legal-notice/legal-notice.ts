5bef671fdfd07a879444c6625f74ebd7c3e845b1
Jana S. <schaaf.jana@outlook.de>
2026-06-16 14:53:16 +0200

    refactoring links in the navbar

--- src/app/pages/legal-notice/legal-notice.ts
+++ src/app/pages/legal-notice/legal-notice.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { Header } from "../../components/header/header";
 
 @Component({
   selector: 'app-legal-notice',
-  imports: [Header],
+  imports: [],
   templateUrl: './legal-notice.html',
   styleUrl: './legal-notice.scss',
 })
