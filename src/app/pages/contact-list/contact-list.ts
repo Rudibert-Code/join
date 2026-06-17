778aafb38840f3c02cb3a6bffdbda15c377ca647
Bjoern <onoofelogmail.com>
2026-06-17 15:54:34 +0200

    - added "contact details" dialog (WIP)

--- src/app/pages/contact-list/contact-list.ts
+++ src/app/pages/contact-list/contact-list.ts
@@ -3,9 +3,11 @@ import { Contacts } from '../../components/contacts/contacts';
 import { ContacsButton } from '../../components/contacs-button/contacs-button';
 import { App } from "../../app";
 
+import { ContactDetails } from '../../components/contact-details/contact-details'
+
 @Component({
   selector: 'app-contact-list',
-  imports: [Contacts,ContacsButton],
+  imports: [Contacts,ContacsButton,ContactDetails],
   templateUrl: './contact-list.html',
   styleUrl: './contact-list.scss',
 })
