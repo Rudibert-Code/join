a30501983d73cb9083f1e5cd85ad05fd4b9e0399
Bjoern <onoofelogmail.com>
2026-06-17 18:07:04 +0200

    "contact details" - connected contact info with corresponding elements - clicking contact now opens pop up with contact details - animated pop up window (fade in & position transform)

--- src/app/components/contact-details/contact-details.ts
+++ src/app/components/contact-details/contact-details.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Supabase } from '../../supabase';
+import { Contacts } from '../../components/contacts/contacts';
 
 @Component({
   selector: 'app-contact-details',
@@ -7,6 +9,23 @@ import { Component } from '@angular/core';
   styleUrl: './contact-details.scss',
 })
 export class ContactDetails {
+  db = inject(Supabase);
+
+  userName:string=""; 
+  userSurName:string="";
+  userEmail:string="";
+  userPhone:string="";
+  userInitials:string="";
+
+  loadDetails(){
+    let testMeh = this.db.contacts();
+    this.userName = testMeh[0].first_name;
+    this.userSurName = testMeh[0].last_name;
+    this.userEmail = testMeh[0].email;
+    this.userPhone = testMeh[0].phone;
+    this.userInitials = (this.userName.charAt(0).toUpperCase())+(this.userSurName.charAt(0).toUpperCase());
+  }
+  
   test(){
     console.log("HI")
   }
--- src/app/components/contacts/contacts.ts
+++ src/app/components/contacts/contacts.ts
@@ -1,5 +1,6 @@
 import { Component, computed, inject } from '@angular/core';
 import { Contact, Supabase } from '../../supabase';
+import { ContactDetails } from '../contact-details/contact-details'
 import { first } from 'rxjs';
 
 @Component({
@@ -31,5 +32,10 @@ export class Contacts {
     const lastNameLetter = contact.last_name.charAt(0).toUpperCase();
 
     return firstNameLetter + lastNameLetter;
+  }
+
+  openContactDetails(){
+    let detailsPopUp = document.getElementById('contactDetails') as HTMLDialogElement;
+    detailsPopUp.classList.toggle('active')
   }
 }
