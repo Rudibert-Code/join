90f243bc3862ee798014b2542f131ba69bdbc02e
Felix Winkler <Felix-Winkler96@web.de>
2026-06-18 15:41:05 +0200

    added connection with responsive button to add contact

--- src/app/components/contacs-button/contacs-button.ts
+++ src/app/components/contacs-button/contacs-button.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { Component, signal, inject } from '@angular/core';
+import { ContactForm } from '../contact-form/contact-form';
+import { Supabase } from '../../supabase';
 
 @Component({
   selector: 'app-contacs-button',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ContactForm],
   templateUrl: './contacs-button.html',
   styleUrl: './contacs-button.scss',
 })
 export class ContacsButton {
+  
+  db = inject(Supabase);
+
+  showContactForm = signal(false);
+
+  openContactForm() {
+    this.showContactForm.set(true);
+  }
+
+  closeContactForm() {
+    this.showContactForm.set(false);
+  }
 }
