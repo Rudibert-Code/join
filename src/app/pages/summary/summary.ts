2dc80788616a2fc1cbd5af787616738bc532c8f3
Jana S. <schaaf.jana@outlook.de>
2026-07-03 14:35:49 +0200

    modify summery page view

--- src/app/pages/summary/summary.ts
+++ src/app/pages/summary/summary.ts
@@ -1,5 +1,7 @@
 import { DatePipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject, computed } from '@angular/core';
+import { Supabase } from '../../supabase';
+import { Task } from '../../supabase';
 
 @Component({
   selector: 'app-summary',
@@ -9,9 +11,39 @@ import { Component } from '@angular/core';
   imports: [DatePipe],
 })
 export class Summary {
+  db = inject(Supabase);
+  tasks = computed(() => this.db.tasks());
+
   deadline: Date = new Date();
 
   constructor() {
     this.deadline.setDate(this.deadline.getDate() + 5);
+  }
+  ngOnInit(){
+    this.loadTasks();
+  }
+
+  async loadTasks(){
+    await this.db.getTasks();
+  }
+
+  get todoTasks() {
+    return this.tasks().filter((task) => this.getStatus(task) === 'to_do');
+  }
+
+  get progressTasks() {
+    return this.tasks().filter((task) => this.getStatus(task) === 'in_progress');
+  }
+
+  get feedbackTasks() {
+    return this.tasks().filter((task) => this.getStatus(task) === 'await_feedback');
+  }
+
+  get doneTasks() {
+    return this.tasks().filter((task) => this.getStatus(task) === 'done');
+  } 
+
+  private getStatus(task: Task): string{
+    return task.status?.toLowerCase().trim() || 'to_do';
   }
 }
