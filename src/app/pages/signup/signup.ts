0c7fa500c4482bed01279b362add52307529f55a
Felix Winkler <Felix-Winkler96@web.de>
2026-07-05 21:09:14 +0200

    added icons in inputfields

--- src/app/pages/signup/signup.ts
+++ src/app/pages/signup/signup.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -7,4 +7,19 @@ import { RouterLink } from "@angular/router";
   templateUrl: './signup.html',
   styleUrl: './signup.scss',
 })
-export class Signup {}
+export class Signup {
+  iconSrc = 'assets/UI/icon_locked.png';
+  private visibilityOnSrc = 'assets/UI/icon_visibility.png';
+  private visibilityOffSrc = 'assets/UI/icon_locked.png';
+  showPassword() {
+    const password = document.getElementById('pwd') as HTMLInputElement | null;
+    if (!password) return;
+    if (password.type === 'password') {
+      password.type = 'text';
+      this.iconSrc = this.visibilityOnSrc;
+    } else {
+      password.type = 'password';
+      this.iconSrc = this.visibilityOffSrc;
+    }
+  }
+}
