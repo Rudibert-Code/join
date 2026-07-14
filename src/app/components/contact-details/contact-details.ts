6cdbb69c0c61a5f3b5c02f542df771ac30e6eeba
Bjoern <onoofelogmail.com>
2026-07-14 12:54:21 +0200

    - splitted resetWindow() into open- & closeWindow() to change pop up behaviour (detail viewer animation resets with each click)

--- src/app/components/contact-details/contact-details.ts
+++ src/app/components/contact-details/contact-details.ts
@@ -72,8 +72,7 @@ export class ContactDetails {
   }
 
   async deleteContact() {
-    //this.resetWindow();
-    //this.loadDetails()
+    this.closeWindow();
 
     if (this.selectedContactId === null) {
       return;
@@ -90,25 +89,52 @@ export class ContactDetails {
     this.userInitials = "";
     this.isEditModalOpen = false;
     },0)
+
+    isActive = this.detailViewActive;
   }
 
- //getFirstEntry(){
- //  let id = this.
- //}
+ getFirstEntry(){
+    this.userName = this.db.contacts()[0].first_name;
+    this.userSurName = this.db.contacts()[0].last_name;
+    this.userEmail = this.db.contacts()[0].email;
+    this.userPhone = this.db.contacts()[0].phone;
+    this.userInitials = (this.userName.charAt(0).toUpperCase())+(this.userSurName.charAt(0).toUpperCase());
+ }
 
-  resetWindow(){
+
+
+  //resetWindow(){
+  //  let detailsPopUp = document.getElementById('contactDetails') as HTMLDialogElement; 
+  //  if (detailsPopUp.classList.contains("active")) {
+  //    detailsPopUp.classList.remove("active");
+  //    detailsPopUp.classList.add("in-active");
+  //    this.detailViewActive = false
+  //  } else{
+  //    detailsPopUp.classList.remove("in-active");
+  //    detailsPopUp.classList.add("active");
+  //    this.detailViewActive = true;
+  //  }
+  //}
+
+
+  
+
+  openWindow(){
     let detailsPopUp = document.getElementById('contactDetails') as HTMLDialogElement; 
     if (detailsPopUp.classList.contains("active")) {
       detailsPopUp.classList.remove("active");
-      detailsPopUp.classList.add("in-active");
-      this.detailViewActive = false
-    } else{
-      detailsPopUp.classList.remove("in-active");
-      detailsPopUp.classList.add("active");
-      this.detailViewActive = true;
     }
-    isActive = this.detailViewActive;
+    setTimeout(()=>{
+    detailsPopUp.classList.add("active");
+    },0)
   }
+
+  closeWindow(){
+    let detailsPopUp = document.getElementById('contactDetails') as HTMLDialogElement; 
+    detailsPopUp.classList.remove("active");
+    detailsPopUp.classList.add("in-active");
+  }
+
 
   changeState(X:boolean){
     this.detailViewActive = X;
